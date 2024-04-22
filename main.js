7c6a73330761a1bf01dd12d85004726f8feaf874
kauã gomes <kgs26121967@gmail.com>
2024-04-22 17:30:36 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+function dizOla(nome) {
+    return "Olá " + nome;
+}
+let nome = "João";
+console.log(dizOla(nome));
+const resultado = multiplicacao(5, 3);
+console.log(resultado);
---
+++ main.ts
@@ -0,0 +1,17 @@
+function multiplicacao(num1: number, num2: number): number{
+
+        return num1 * num2;
+
+}
+
+function dizOla(nome: string ): string {
+    return "Olá " + nome;
+}
+
+let nome= "João";
+
+console.log(dizOla(nome));
+
+
+const resultado = multiplicacao(5, 3);
+console.log(resultado);
